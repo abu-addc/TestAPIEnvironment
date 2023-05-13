466ba0281b16535f18a6b1d36d846d8c6262690b
aldomedrano2201 <aldomedrano2201@gmail.com>
2023-05-13 11:45:40 -0400

    User model definition based on the mongodb users document

--- Models/User.py
+++ Models/User.py
@@ -1,1 +1,12 @@
 ### Felipe and Aldo
+class User:
+    def __init__(self, user_id, username, password, email, name, job_title, phone_number, date_registered, access_level):
+        self.user_id = user_id
+        self.username = username
+        self.password = password
+        self.email = email
+        self.name = name
+        self.job_title = job_title
+        self.phone_number = phone_number
+        self.date_registered = date_registered
+        self.access_level = access_level
