7179bb0edb7ce094bd10319ef79178f632c51a09
aldomedrano2201 <aldomedrano2201@gmail.com>
2023-05-14 21:13:09 -0400

    added invetory update status end point

--- Controllers/InventoriesController.py
+++ Controllers/InventoriesController.py
@@ -32,3 +32,23 @@ def get_inventories_by_user(user_id):
     # Serialize inventories to JSON with the custom encoder
     inventories_json = json.dumps(inventories, cls=CustomEncoder)
     return inventories_json
+
+# Update inventory status by inventory_id and status value
+def update_inventory_status():
+
+     # Get the inventory ID from the query parameters
+    inventory_id = request.args.get('inventory_id')
+
+    # Get the new status from the request body
+    new_status = request.json.get('status')
+
+    # Update the status field of the inventory document
+    result = collection.update_one(
+        {"inventory_id": inventory_id},
+        {"$set": {"status": new_status}}
+    )
+
+    if result.matched_count == 1:
+        return jsonify({'message': 'Inventory status updated successfully'})
+    else:
+        return jsonify({'message': 'Inventory not found'}), 404
--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from pymongo import MongoClient
-from Controllers.InventoriesController import get_inventories_by_user
+from Controllers.InventoriesController import *
 
 
 app = Flask(__name__)
 
 @app.route('/inventories/<user_id>', methods=['GET'])
-def inventories_route(user_id):
+def inventories_route_get_inventories(user_id):
     return get_inventories_by_user(user_id)
+
+@app.route('/updateInventories', methods=['PUT'])
+def inventories_route_update_inventory_status():
+    return update_inventory_status()
 
 
 if __name__ == '__main__':
